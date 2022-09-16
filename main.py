146ddafaf50c9b66fde3193d4dae749b7bf017fe
Fares Manai <83595215+Kefrov@users.noreply.github.com>
2022-09-16 15:54:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,55 @@ from math import *
 win = Tk()
 
 
-def draw():
-    global pts, stop_draw, app
+def create_canvas():
+    global step, cp, n_units
 
-    if entry.get() != '':
-        b_apply.configure(text='clear')
-    if app != '':
-        clear_entry()
-        b_apply.configure(text='apply')
+    z, y = 0, 0
+    for i in range(n_units):
+        cp.append(canvas.create_line(z, 0, z, ch, fill='gray', dash=(4, 2)))
+        cp.append(canvas.create_line(0, y, ch, y, fill='gray', dash=(4, 2)))
+        z += step
+        y += step
+    cp.append(canvas.create_line(int(c_half) + 1, ch, int(c_half) + 1, 1, fill='white', arrow=LAST))
+    cp.append(canvas.create_line(0, int(c_half) + 1, ch - 1, int(c_half) + 1, fill='white', arrow=LAST))
+
+
+
+def draw(zoom):
+    global pts, app, step, n_units
+
+    if not zoom:
+        if entry.get() != '':
+            b_apply.configure(text='clear')
+        if app != '':
+            clear_entry()
+            b_apply.configure(text='apply')
 
     app = entry.get()
-    for i in range(-int(c_half) + 1, int(c_half) + 1):
-        x1 = i / step
-        x2 = (i + 1) / step
+    z1 = -n_units - 0.02777
+    z2 = -n_units
+    for i in range(-int(n_units / 0.02777), int(n_units / 0.02777)):
+        z1 += 0.02777
+        z2 += 0.02777
         try:
-            y1 = eval(app.replace('x', f'({str(x1)})'))
-            y2 = eval(app.replace('x', f'({str(x2)})'))
+            y1 = eval(app.replace('z', f'({str(z1)})'))
+            y2 = eval(app.replace('z', f'({str(z2)})'))
                 
-            if -20 < y1 < 20 and not stop_draw:
-                pts.append(canvas.create_line(i + c_half, -(y1 * step) + c_half, 
-                i + 1 + c_half, -(y2 * step) + c_half, fill='red', width=3))
-                win.after(1, canvas.update())   
+            if -n_units < y1 < n_units:
+                pts.append(canvas.create_line(z1 * step + c_half, -(y1 * step) + c_half, 
+                z2 * step + c_half, -(y2 * step) + c_half, fill='red', width=3))  
         except Exception:  
              pass
 
-        stop_draw = False
-
 
 def clear_entry():
-    global pts, stop_draw, point
+    global pts, point
 
     for i in pts:
         canvas.delete(i)
 
-    stop_draw = True
     entry.delete(0, 'end')
-    x_input.delete(0, 'end')
+    z_input.delete(0, 'end')
     y_value.configure(text='')
     canvas.delete(point)
     app = ''
@@ -52,24 +65,46 @@ def calc():
     canvas.delete(point)
 
     try:
-        x = x_input.get()
-        y = round(eval(app.replace('x', f'({str(x)})')), 3)
+        z = z_input.get()
+        y = round(eval(app.replace('z', f'({str(z)})')), 3)
         y_value.configure(text=str(int(y) if y == int(y) else y))
-        point = canvas.create_oval(float(x) * step + c_half - 5, -(y * step) + c_half - 5, 
-                float(x) * step + c_half + 5, -(y * step) + c_half + 5, 
+        point = canvas.create_oval(float(z) * step + c_half - 5, -(y * step) + c_half - 5, 
+                float(z) * step + c_half + 5, -(y * step) + c_half + 5, 
                 outline='green', fill='light green')
     except Exception:
         pass
+
+
+def zoom(sign):
+    global step, n_units, point, pts, cp
+
+    canvas.delete(point)
+    for i in pts:
+        canvas.delete(i)
+
+    if sign == '+' and n_units > 6:
+        n_units -= 2
+        step = ch / n_units
+    elif sign == '-' and n_units < 36:
+        n_units += 2
+        step = ch / n_units
+
+    for i in cp:
+        canvas.delete(i)
+
+    create_canvas()
+    draw(True)
+    calc()
     
 
-sw = win.winfo_screenwidth()
-ww = int(sw / 1.5)
-ch = int(sw / 2.2)
+ww = int(1600 / 1.5)
+ch = int(1600 / 2.2) 
 c_half = ch / 2
-step = ch / 18
+n_units = 18
+step = ch / n_units
 
 pts = []
-stop_draw = False
+cp = []
 point = ''
 app = ''
 
@@ -85,38 +120,39 @@ canvas.pack(side='right')
 credit = Label(win, bg='black', fg='white', font=('Microsoft Himalaya', 25), text='- Made by Fares -')
 credit.pack()
 
-entry_text = Label(text='f(x) = ', font=('Microsoft Himalaya', 30), bg='black', fg='white')
+entry_text = Label(text='f(z) = ', font=('Microsoft Himalaya', 30), bg='black', fg='white')
 entry = Entry(win, bg='black', fg='white', cursor='ibeam', font=('Microsoft Himalaya', 30), 
         width=16, borderwidth=0)
-entry.place(relx=0.19, rely=0.2, anchor= CENTER)
+entry.place(relx=0.19, rely=0.2, anchor=CENTER)
 entry_text.place(relx=0.06, rely=0.2, anchor=CENTER)
 
-b_apply = Button(win, bg='black', fg='white', cursor='hand2', text='apply', 
-          font=('Microsoft Himalaya', 25), width=5, command=lambda: draw())
-b_apply.place(relx=0.159, rely=0.3, anchor= CENTER)
+b_apply = Button(win, bg='black', fg='light green', cursor='hand2', text='apply', 
+          font=('Microsoft Himalaya', 25), width=5, command=lambda: draw(False))
+b_apply.place(relx=0.159, rely=0.3, anchor=CENTER)
 
-x_input_text = Label(text='x = ', font=('Microsoft Himalaya', 30), bg='black', fg='white')
-x_input = Entry(win, bg='black', fg='white', cursor='ibeam', font=('Microsoft Himalaya', 30), 
+z_input_text = Label(text='z = ', font=('Microsoft Himalaya', 30), bg='black', fg='white')
+z_input = Entry(win, bg='black', fg='white', cursor='ibeam', font=('Microsoft Himalaya', 30), 
           width=16, borderwidth=0)
-x_input.place(relx=0.18, rely=0.6, anchor= CENTER)
-x_input_text.place(relx=0.06, rely=0.6, anchor= CENTER)
+z_input.place(relx=0.18, rely=0.6, anchor=CENTER)
+z_input_text.place(relx=0.06, rely=0.6, anchor=CENTER)
 
 y_text = Label(text='y = ', font=('Microsoft Himalaya', 30), bg='black', fg='white')
 y_value = Label(font=('Microsoft Himalaya', 30), bg='black', fg='white')
 y_value.place(relx=0.08, rely=0.7, anchor='w')
-y_text.place(relx=0.06, rely=0.7, anchor= CENTER)
+y_text.place(relx=0.06, rely=0.7, anchor=CENTER)
 
-b_calc = Button(win, bg='black', fg='white', cursor='hand2', text='apply', 
+b_calc = Button(win, bg='black', fg='light green', cursor='hand2', text='apply', 
          font=('Microsoft Himalaya', 25), width=5, command=lambda: calc())
-b_calc.place(relx=0.159, rely=0.8, anchor= CENTER)
+b_calc.place(relx=0.159, rely=0.8, anchor=CENTER)
 
-x, y = 0, 0
-for i in range(18):
-    canvas.create_line(x, 0, x, ch, fill='gray', dash=(4, 2))
-    canvas.create_line(0, y, ch, y, fill='gray', dash=(4, 2))
-    x += step
-    y += step
-y_axis = canvas.create_line(int(c_half) + 1, ch, int(c_half) + 1, 1, fill='white', arrow=LAST)
-x_axis = canvas.create_line(0, int(c_half) + 1, ch - 1, int(c_half) + 1, fill='white', arrow=LAST)
+zoom_in = Button(win, bg='black', fg='light green', cursor='hand2', text='+',
+          font=('Microsoft Himalaya', 25), width=4, command=lambda: zoom('+'))
+zoom_in.place(relx=0.932, rely=0.9)
+
+zoom_out = Button(win, bg='black', fg='light green', cursor='hand2', text='-',
+          font=('Microsoft Himalaya', 25), width=4, command=lambda: zoom('-'))
+zoom_out.place(relx=0.932, rely=0.8)
+
+create_canvas()
 
 win.mainloop()
